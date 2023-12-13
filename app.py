e984ff646271a0a85c872298e5369ccb069e6202
Amit Gurmukhani <112573512+amitg27@users.noreply.github.com>
2023-12-13 13:29:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from flask import send_from_directory
 
 app = flask.Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
+
 
 @app.route('/')
 @app.route('/home')
